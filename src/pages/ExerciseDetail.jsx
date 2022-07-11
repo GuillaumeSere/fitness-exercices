5b54b324e0d1d2268a0454d1b32fa8329ceda81c
Guillaume Sere <guillaumesere@hotmail.fr>
2022-07-11 18:29:10 +0200

    change key api

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -16,13 +16,10 @@ const ExerciseDetail = () => {
     useEffect(() => {
         const fetchExercisesData = async () => {
             const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-            const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
-
+            
             const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
             setExerciseDetail(exerciseDetailData);
 
-            const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
-            setExerciseVideos(exerciseVideosData.contents)
 
             const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
             setTargetMuscleExercises(targetMuscleExercisesData);
